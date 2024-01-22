66a1e1aab044a19585f87ed382de5b91edbfeab0
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2024-01-22 15:08:50 -0800

    Initial Commit

---
+++ HomeHarbor/HomeHarborApp.swift
@@ -0,0 +1,20 @@
+//
+//  HomeHarborApp.swift
+//  HomeHarbor
+//
+//  Created by Amulya Gangam on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeHarborApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
